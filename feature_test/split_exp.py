adec670a7c51a27604bd7845bced8a8166a38920
sudoskys <coldlando@hotmail.com>
2024-01-18 23:39:23 +0800

    feat: add split_exp.py for testing split method
    This commit adds the file split_exp.py, which is used for testing the split method. The method splits a string using "2" as the delimiter and assigns the result to sub_key and key variables. The split_exp.py file also includes a print statement to display the values of sub_key and key.

---
+++ feature_test/split_exp.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/1/18 下午11:09
+# @Author  : sudoskys
+# @File    : split_exp.py
+
+method = "ass2srt"
+sub_key, key = method.split("2", maxsplit=1)
+
+print(sub_key, key)
